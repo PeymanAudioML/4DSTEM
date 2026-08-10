f566c49dd80d038b98c3aff2c8cce36774b1d15a
PeymanAudioML <poozesh.peyman@gmail.com>
2026-08-10 16:24:46 +0000

    Scaffold CLI entry point and ReconsPlan dataclass

---
+++ phaser/main.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+import sys
+import typing as t
+
+import click
---
+++ phaser/plan.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import typing as t
+
+from .types import Dataclass
+
+
+class ReconsPlan(Dataclass, kw_only=True):
+     pass 
+
+
+
+     
+     
