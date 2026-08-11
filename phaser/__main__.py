931d09666e9a8dc2b60331b949ad2b3e9298def5
peymanML <200176817+peymanML@users.noreply.github.com>
2026-08-11 17:31:54 +0000

    Revert "Merge pull request #2 from PeymanAudioML/peyman"
    This reverts commit d8b0a7180674e5ec742cb3193361736dc25f1696, reversing
    changes made to 1f5c253f550bcfb5c9a273f1a99f48ceb0a53407.

--- phaser/__main__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-if __name__ == '__main__':
-    from phaser.main import cli
-    cli()
--- phaser/main.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-import sys
-import typing as t
-
-import click
